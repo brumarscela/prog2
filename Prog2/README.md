885db6a4bfb15c1279990ba39e564719b8bfa12c
Bruna Marcela <47820060+brumarscela@users.noreply.github.com>
2019-03-20 10:02:27 -0300

    Add files via upload
    Desenvolvimento de exercícios do conteúdo das últimas aulas.

---
+++ Prog2/README.md
@@ -0,0 +1,10 @@
+# Prog2
+Repositório contendo os aquivos de Programação 2...
+----Vamos ver se o merge rola ----
+Testando as funções da aula 2!
+<<<<<<< HEAD
+
+Trigésima Tentativa
+=======
+Vamo denovo
+>>>>>>> d1158d7d0360306c62b0778534c9f61aca7c8fe5
---
+++ Prog2/TiagoStats/server.py
@@ -0,0 +1,25 @@
+from flask import Flask , render_template
+
+app = Flask(__name__) #, static_url_path = '/ static')
+
+@app.route("/")
+def inicio():
+    return render_template ("Inicio.html")
+
+@app.route("/listarpessoa")
+def listarPessoas():
+    return render_template ("ListarPessoa.html")
+
+@app.route("/inserirpessoa")
+def inserirPessoas():
+    return render_template ("InserirPessoa.html")
+
+@app.route("/alterarpessoa")
+def alterarPessoa():
+    return render_template ("AlterarPessoa.html")
+
+@app.route("/exibirmensagem")
+def exibirMensagem():
+    return render_template ("ExibirMensagem.html")
+
+app.run(debug=True)
