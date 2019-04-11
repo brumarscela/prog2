356600e38a284ee1122b1aba8bb447b2ee4c65d0
Bruna Marcela <47820060+brumarscela@users.noreply.github.com>
2019-04-11 11:13:36 -0300

    Add files via upload
    teste

---
+++ servidorwebcadastrodinamico.py
@@ -0,0 +1,17 @@
+"""exercício página 29
+
+Em vez de utilizar links para disponibilizar a operac¸ao de exclus ˜ ao de registros, deseja-se utilizar ˜
+botoes, de forma que ao lado dos dados de cada pessoa apare ˜ c¸a um botao (no lugar de um link) ˜
+titulado “Excluir”. Na tela de listagem de registros (Codigo ´ 3.17), substitua o codigo do link de ´
+exclusao (linha 12) por um c ˜ odigo que fac¸a uso de bot ´ ao para acionar a exclus ˜ ao. ˜
+
+
+2. Considere uma ac¸ao chamada ˜ form alterar pessoa, responsavel por abrir um formul ´ ario ´
+contendo os dados de uma pessoa. Tal ac¸ao deve receber um par ˜ ametro chamado ˆ cpf, cujo
+valor deve ser o identificador contendo apenas os numeros do CPF de uma pessoa. Forne ´ c¸a o
+codigo HTML completo do link que leva ´ a tela de altera ` c¸ao de dados de uma pessoa que possui o ˜
+seguinte CPF (apenas para demonstrac¸ao): 123.456.789-10. ˜
+Apos tentar resolver as quest ´ oes, verifique respostas na p ˜ agina ´ 54.
+
+
+"""
