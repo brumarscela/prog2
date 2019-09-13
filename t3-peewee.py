62ca77f284756743232ff41daea257d3360effdb
Bruna Marcela <47820060+brumarscela@users.noreply.github.com>
2019-09-13 10:10:31 -0300

    Add files via upload

---
+++ t3-peewee.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify
+from modelo import Pessoa
+from playhouse.shortcuts import model_to_dict, dict_to_model
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return "backend do sistema de pessoas; <a href=/listar_pessoas>API listar pessoas</a>"
+
+@app.route("/listar_pessoas")
+def listar_pessoas():
+    # forma alternativa rápida: usando map (lambda)
+    pessoas = list(map(model_to_dict, Pessoa.select()))
+    return jsonify({'lista':pessoas})
+    # referência: 
+    # https://www.geeksforgeeks.org/python-map-function/
+    
+''' exemplo do map:
+def dobrar(n):
+    return n*2
+numeros = [1, 2, 3]
+resultado = map(dobrar, numeros)
+print(resultado)
+print(list(resultado))
+execução:
+<map object at 0x7ff0ca2d6a90>
+[2, 4, 6]
+'''
+
+
+'''
+exemplo de retorno:
+{
+  "lista": [
+    {
+      "endereco": "Casa 9", 
+      "id": 1, 
+      "nome": "Joao da Silva", 
+      "telefone": "3541-1230"
+      "email": "joao.silva@gmail.com"
+    }, 
+    {
+      "endereco": "Casa 9", 
+      "id": 2, 
+      "nome": "Joao da Silva", 
+      "telefone": "3541-1230"
+      "email": "joao.s@gmail.com"
+    }
+  ]
+}
+'''
+
+app.run(debug=True,port=4999)
